2c62c756a4c6037effe4509a830b5b98c4ea50fd
Javier <javicuba11@gmail.com>
2015-06-11 17:35:31 +0200

    Added new icons

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -2,5 +2,21 @@
 'use strict';
 
 app.controller('AppCtrl', function($scope) {
-
+  $scope.menu = [{
+    name: 'Practica Solo',
+    href: '#/app/bases',
+    icon: 'icon-solo'
+  }, {
+    name: 'Batalla',
+    href: '#/',
+    icon: 'icon-batalla'
+  }, {
+    name: 'Palabras',
+    href: '#/',
+    icon: 'icon-palabras'
+  }, {
+    name: 'Rap\'s',
+    href: '#/app/raps',
+    icon: 'icon-raps'
+  }];
 });
