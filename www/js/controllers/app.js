6653b8cc62358d25fcde0d7b354bd19298e14c8d
Javier <javicuba11@gmail.com>
2015-07-07 17:22:22 +0200

    style(home): Menu changed
    Change the home menu style

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -5,18 +5,22 @@ app.controller('AppCtrl', function($scope) {
   $scope.menu = [{
     name: 'Practica Solo',
     href: '#/app/bases',
-    background: 'alone'
+    background: 'img/alone.jpg',
+    mp4: 'img/alone.mp4'
   }, {
     name: 'Batalla',
     href: '#/',
-    background: 'battle'
+    background: 'img/battle.jpg',
+    mp4: 'img/battle.mp4'
   }, {
     name: 'Palabras',
     href: '#/',
-    background: 'words'
+    background: 'img/words.jpg',
+    mp4: 'img/words.mp4'
   }, {
     name: 'Rap\'s',
     href: '#/app/raps',
-    background: 'raps'
+    background: 'img/raps.jpg',
+    mp4: 'img/raps.mp4'
   }];
 });
